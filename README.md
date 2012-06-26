0ab57c99a46f33437d63a412d8aa62320f3e2f0e
boostbob <1982wb@gmail.com>
2012-06-26 17:29:31 +0800

    update README

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ password-generator
 ==================
 
 A very simple password generator.
+
+```ruby
+>> require 'password-generator'
+=> true
+>> PasswordGenerator.new_password
+=> "{XO55fGXuPFK"
+>> PasswordGenerator.new_password
+=> "u#S9s{)BbOkj"
+>> PasswordGenerator.new_password
+=> "g<A{Q:I-[+;K">
+```
--- lib/password-generator.rb
+++ lib/password-generator.rb
@@ -2,7 +2,7 @@ require "password-generator/version"
 
 class PasswordGenerator
 	def self.new_password(size = 12)
-			chars = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a + ['!','@','#','$','%','^','*','&','(',')','-','+']) - %w(i o 0 1 l 0)
+			chars = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a + ['!','@','#','$','%','^','*','&','(',')','-','+',',',';','?','/','}','{','[',']','=','<','>', ':']) - %w(i o 0 1 l 0)
 			(1..size).collect{|a| chars[rand(chars.size)] }.join
 	end
 end
